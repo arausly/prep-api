2076730a37e9d9ab6ead13ecf5b2270ee98bf451
arausly <arausid@yahoo.com>
2025-08-25 13:29:58 +0100

    starting a session with default message

--- src/routes/session.py
+++ src/routes/session.py
@@ -29,6 +29,30 @@ def create_session(interview_session: Session):
         )
         if response.data is None or not bool(len(response.data)):
             raise HTTPException(status_code=500, detail="Failed to create session")
+        # create default interviewer message
+        interviewer_response = (
+            supabase.table(Table.interview_conversations)
+            .insert(
+                {
+                    "session_id": response.data[0]["id"],
+                    "type": "interviewer",
+                    "content": (
+                        "Hello! I'm your interviewer and I'll be conducting your interview"
+                        f"for the {interview_session.job_position}"
+                        f"position at {interview_session.company}.",
+                        "Let's start with a simple question: Can you tell me a"
+                        "bit about yourself and why you're interested in this role?",
+                    ),
+                }
+            )
+            .execute()
+        )
+        if interviewer_response.data is None or not bool(
+            len(interviewer_response.data)
+        ):
+            raise HTTPException(
+                status_code=500, detail="Failed to create default interviewer message"
+            )
         return JSONResponse(
             status_code=201,
             content={"message": "session created", "session": response.data[0]},
